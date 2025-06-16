0c275d34e5bdfac910efedd7a319f1b8eb5734d4
paulk4100 <paulkim@cpp.edu>
2025-06-16 13:51:34 -0700

    Update switch_led_interface.v

--- Group E/ECE3300-Lab1-GroupE/switch_led_interface.v
+++ Group E/ECE3300-Lab1-GroupE/switch_led_interface.v
@@ -1,6 +1,6 @@
 module switch_led_interface(
-    input wire [15:0] sw,  // declaration of 16 bit wide input named sw of data type wire
-    output wire [15:0] led // declaration of 16 bit wide output named led of data type wire
+    input wire [15:0] sw,  
+    output wire [15:0] led 
     );
     
     assign led = sw; 
