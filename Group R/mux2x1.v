7991dc93759ba930157857738c75472ca840668c
Dia Agrawal <61632682+dia-agrawal@users.noreply.github.com>
2025-06-27 17:59:26 -0700

    Add files via upload

---
+++ Group R/mux2x1.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/27/2025 03:10:24 PM
+// Design Name: 
+// Module Name: mux2x1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2x1 (
+                input a, b,
+                input sel,
+                output y
+              );
+
+wire nsel, a1, b1;
+
+not (nsel, sel);
+and (a1, a, nsel);
+and (b1, b, sel);
+or (y, a1, b1);
+
+endmodule
