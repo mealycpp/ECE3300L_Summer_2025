07ea700b3a0460c8cf3657ccb519a640765a9f43
paulk4100 <paulkim@cpp.edu>
2025-06-30 11:29:30 -0700

    Update mux2x1.v

--- Group E/ECE3300-Lab3-GroupE/mux2x1.v
+++ Group E/ECE3300-Lab3-GroupE/mux2x1.v
@@ -21,14 +21,16 @@
 
 
 module mux2x1 (
-input a, b,
-input sel,
-output y
-);
+                input a, b,
+                input sel,
+                output y
+               );
+               
 wire nsel, a1, b1;
+
 not (nsel, sel);
 and (a1, a, nsel);
 and (b1, b, sel);
 or (y, a1, b1);
-endmodule
 
+endmodule
