0244cf650d693f6a5671555a34e8299c54ba2dcb
paulk4100 <paulkim@cpp.edu>
2025-06-30 11:24:08 -0700

    Update mux2x1.v

--- Group E/ECE3300-Lab3-GroupE/mux2x1.v
+++ Group E/ECE3300-Lab3-GroupE/mux2x1.v
@@ -1,1 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly Pomona
+// Engineer: ECE
+// 
+// Create Date: 06/30/2025 11:21:38 AM
+// Design Name: 
+// Module Name: mux2x1
+// Project Name: mux2x1
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2x1 (
+input a, b,
+input sel,
+output y
+);
+wire nsel, a1, b1;
+not (nsel, sel);
+and (a1, a, nsel);
+and (b1, b, sel);
+or (y, a1, b1);
+endmodule
 
