1bb48c73b609aba3b9b89a567b915411013874d5
Evan Tram <etram13@gmail.com>
2025-07-01 15:36:39 -0700

    Create debounce.v

---
+++ Group W/ECE3300-Lab3-GroupW/debounce.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module debounce (
+    input clk,
+    input btn_in,
+    output reg btn_clean
+    );
+    
+    reg [2:0] shift_reg;
+    always @(posedge clk) 
+        begin
+            shift_reg <= {shift_reg[1:0], btn_in};
+            
+            if (shift_reg == 3'b111) 
+                btn_clean <= 1;
+            else if (shift_reg == 3'b000) 
+                btn_clean <= 0;
+        end
+    
+endmodule
