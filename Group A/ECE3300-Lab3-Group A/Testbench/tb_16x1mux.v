f00a4dd7412b11b8f1c51731615700a30da5cbe8
EdwinEstrada102 <87724218+EdwinEstrada102@users.noreply.github.com>
2025-06-30 23:58:32 -0700

    Add files via upload

--- Group A/ECE3300-Lab3-Group A/Testbench/tb_16x1mux.v
+++ Group A/ECE3300-Lab3-Group A/Testbench/tb_16x1mux.v
@@ -37,26 +37,249 @@ module tb_16x1mux(
         forever #5 clk = ~clk; // Toggle clock every 5 time units (10 time units full period)
     end
     initial begin
+    //sel 0
+        SW = 16'h0001;
+        btnD = 0;
+        btnU = 0;
+        btnL = 0;
+        btnR = 0;
         rst = 1;
         #20
         rst = 0;
         
-        SW = 16'h0008;
-        rst = 0;
+        //sel 1
+        SW = 16'h0002;
         btnD = 1;
+        #40
+        btnD = 0;
+        #40
+        btnD = 1;
+        #40
+        btnD = 0;
+        rst = 0;
+        #40
+        
+        //sel 2
+        SW = 16'h0004;
+        btnR = 1;
+        #40
+        btnR = 0;
+        #40
+        btnR = 1;
+        #40
+        btnR = 0;
+        #40
+        
+        //sel 3
+        SW = 16'h0008;
+        btnR = 1;
+        btnD = 1;
+        #40
+        btnR = 0;
+        btnD = 0;
+        #40
+        btnR = 1;
+        btnD = 1;
+        #40
+        btnR = 0;
+        btnD = 0;
+        #40
+        
+        //sel 4
+        SW = 16'h0010;
+        btnL = 1;
+        #40
+        btnL = 0;
+        #40
+        btnL = 1;
+        #40
+        btnL = 0;
+        #40
+        
+        //sel 5
+        SW = 16'h0020;
+        btnL = 1;
+        btnD = 1;
+        #40
+        btnL = 0;
+        btnD = 0;
+        #40
+        btnL = 1;
+        btnD = 1;
+        #40
+        btnL = 0;
+        btnD = 0;
+        #40
+        
+        //sel 6
+        SW = 16'h0040;
+        btnL = 1;
+        btnR = 1;
+        #40
+        btnL = 0;
+        btnR = 0;
+        #40
+        btnL = 1;
+        btnR = 1;
+        #40
+        btnL = 0;
+        btnR = 0;
+        #40
+        
+        //sel 7
+        SW = 16'h0080;
+        btnD = 1;
+        btnL = 1;
+        btnR = 1;
+        #40
+        btnD = 0;
+        btnL = 0;
+        btnR = 0;
+        #40
+        btnD = 1;
+        btnL = 1;
+        btnR = 1;
+        #40
+        btnD = 0;
+        btnL = 0;
+        btnR = 0;
+        #40
+        
+        //sel 8
+        SW = 16'h0100;
+        btnU = 1;
+        #40
+        btnU = 0;
+        #40
+        btnU = 1;
+        #40
+        btnU = 0;
+        #40
+        
+        //sel 9
+        SW = 16'h0200;
+        btnU = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnD = 0;
+        #40
+        btnU = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnD = 0;
+        #40
+        
+        //sel 10
+        SW = 16'h0400;
+        btnU = 1;
+        btnR = 1;
+        #40
+        btnU = 0;
+        btnR = 0;
+        #40
+        btnU = 1;
+        btnR = 1;
+        #40
+        btnU = 0;
+        btnR = 0;
+        #40
+        
+        //sel 11
+        SW = 16'h0800;
+        btnU = 1;
+        btnR = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnR = 0;
+        btnD = 0;
+        #40
+        btnU = 1;
+        btnR = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnR = 0;
+        btnD = 0;
+        #40
+        
+        //sel 12
+        SW = 16'h1000;
+        btnU = 1;
+        btnL = 1;
+        #40
         btnU = 0;
         btnL = 0;
-        btnR = 1;
-        #100
-        btnD = 0;
-        btnR = 0;
-        #100
+        #40
+        btnU = 1;
+        btnL = 1;
+        #40
+        btnU = 0;
+        btnL = 0;
+        #40
+        
+        //sel 13
+        SW = 16'h2000;
+        btnU = 1;
+        btnL = 1;
         btnD = 1;
-        btnR = 1;
-        #100
+        #40
+        btnU = 0;
+        btnL = 0;
         btnD = 0;
+        #40
+        btnU = 1;
+        btnL = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnL = 0;
+        btnD = 0;
+        #40
+        
+        //sel 14
+        SW = 16'h4000;
+        btnU = 1;
+        btnL = 1;
+        btnR = 1;
+        #40
+        btnU = 0;
+        btnL = 0;
         btnR = 0;
-        #100 $finish;
+        #40
+        btnU = 1;
+        btnL = 1;
+        btnR = 1;
+        #40
+        btnU = 0;
+        btnL = 0;
+        btnR = 0;
+        #40
+        
+        //sel 15
+        SW = 16'h8000;
+        btnU = 1;
+        btnL = 1;
+        btnR = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnL = 0;
+        btnR = 0;
+        btnD = 0;
+        #40
+        btnU = 1;
+        btnL = 1;
+        btnR = 1;
+        btnD = 1;
+        #40
+        btnU = 0;
+        btnL = 0;
+        btnR = 0;
+        btnD = 0;
+        #10 $finish;
     end
     
 endmodule
