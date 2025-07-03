c32775667e35a5096368f7ea21469d523995b2a4
keevanese <kqtang@cpp.edu>
2025-07-02 19:29:18 -0700

    Create mux2x1.v

---
+++ Group Q/ECE3300-Lab3-GroupQ/Sources/mux2x1.v
@@ -0,0 +1,11 @@
+module mux2x1 (
+    input a, b,
+    input sel,
+    output y
+);
+    wire nsel, a1, b1;
+    not (nsel, sel);
+    and (a1, a, nsel);
+    and (b1, b, sel);
+    or (y, a1, b1);
+endmodule
