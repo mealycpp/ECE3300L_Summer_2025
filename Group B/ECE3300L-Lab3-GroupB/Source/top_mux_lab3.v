0c0f7bb93d7672e72f07e19c13f65e02ec9ffd7b
fariskhan99 <37455620+fariskhan99@users.noreply.github.com>
2025-07-02 23:08:25 -0700

    Add files via upload

---
+++ Group B/ECE3300L-Lab3-GroupB/Source/top_mux_lab3.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/02/2025 02:21:14 PM
+// Design Name: 
+// Module Name: top_mux_lab3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top_mux_lab3 (
+        input clk,
+        input rst,
+        input [15:0] sw,
+        input btnU, btnD, btnL, btnR,
+        output LED0
+    );
+    wire [3:0] sel;
+    
+    toggle_switch t0 (.clk(clk), .rst(rst), .btn_raw(btnD), .state(sel[0]));
+    toggle_switch t1 (.clk(clk), .rst(rst), .btn_raw(btnR), .state(sel[1]));
+    toggle_switch t2 (.clk(clk), .rst(rst), .btn_raw(btnL), .state(sel[2]));
+    toggle_switch t3 (.clk(clk), .rst(rst), .btn_raw(btnU), .state(sel[3]));
+    
+    mux16x1 mux (.in(sw), .sel(sel), .out(LED0));
+endmodule
