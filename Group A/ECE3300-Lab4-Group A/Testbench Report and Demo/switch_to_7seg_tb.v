2818cfecc53d94313f442dbc92b7c9f7f7ca6238
smiish <mllau@cpp.edu>
2025-07-10 10:27:12 -0700

    Update switch_to_7seg_tb.v

--- Group A/ECE3300-Lab4-Group A/Testbench Report and Demo/switch_to_7seg_tb.v
+++ Group A/ECE3300-Lab4-Group A/Testbench Report and Demo/switch_to_7seg_tb.v
@@ -5,7 +5,7 @@
 // 
 // Create Date: 07/07/2025 07:38:22 AM
 // Design Name: 
-// Module Name: decoderTB
+// Module Name: swtich_to7seg_tb
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -29,7 +29,12 @@ module swtich_to7seg_tb(
     wire [7:0] AN;
     wire [10:0]ledOutput;
 
-    reg [10:0] SW_in = 11'b0; // SW_in[10] = rst_n, SW_in[9:8] = switchPart, SW_in[0:7] = partValue;
+        // rst_n:       SW_in[10]
+        // selector:   SW_in[9:8]
+        // value:       SW_in[7:0]
+    reg [10:0] SW_in = 11'b0; 
+    reg [1:0] select;
+    reg [7:0] partValue;
     
     MainProgram test(
         .clk(clk), 
@@ -51,15 +56,17 @@ module swtich_to7seg_tb(
         #10 SW_in[10] = 1;
         
         for(integer i=0; i<4; i=i+1) begin
-            for(integer j=0; j<15; j=j+1) begin
-                // SW_in[10]=1, SW_in[9:8] = i, SW_in[7:0] = j;
-                //
-                // rst_n:       SW_in[10]
-                // selector:   SW_in[9:8]
-                // value:       SW_in[7:0]
-                SW_in = {1, i[1:0], j[7:0]}; 
-                $display("rst_n=%b | switchPart=%b | partValue=%0d", 
-                         SW_in[10], SW_in[9:8], SW_in[7:0]);
+            select = i[1:0];
+            for(integer j=0; j<=15; j=j+1) begin
+                partValue[3:0] = j[3:0];
+                for(integer k=0; k<=15; k=k+1) begin
+                    partValue[7:4] = k[3:0];
+                    // SW_in[10]=1, SW_in[9:8] = i, SW_in[7:4] = k, SW_in[3:0] = j;
+                    SW_in = {1, select, partValue}; 
+                    $display("rst_n=%b | switchPart=%b | partValue=%h", 
+                             SW_in[10], SW_in[9:8], SW_in[7:0]);
+                    #10;
+                end
                 #10;
             end
         end
