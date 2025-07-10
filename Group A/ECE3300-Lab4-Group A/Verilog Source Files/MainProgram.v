c5a57e82a72621cbd53872d8cefe8fa99c68ef32
smiish <mllau@cpp.edu>
2025-07-10 09:07:55 -0700

    Update MainProgram.v

--- Group A/ECE3300-Lab4-Group A/Verilog Source Files/MainProgram.v
+++ Group A/ECE3300-Lab4-Group A/Verilog Source Files/MainProgram.v
@@ -22,19 +22,18 @@
 
 module MainProgram(
     input clk,
-    input rst_n,
-    input [9:0] switchInput,
-    output [9:0] LEDoutput,
-    output [6:0] Cnode,
-    output dp,
-    output [7:0] AN
+    input [10:0] SW_in,     // [10] rst_n, [9:8] digit, [7:0] input
+    output [10:0] led,
+    output [6:0] Cnode,     // 7-seg [a-g]
+    output dp,              
+    output [7:0] AN         // 7-seg enable lines
 );
 
     wire [31:0] SW;
     
-    Decoder10to32 dec(.selectPart(switchInput[9:8]), .partValue(switchInput[7:0]), .out(SW));
-    seg7_driver driver(.clk(clk), .rst_n(rst_n), .SW(SW), .Cnode(Cnode), .dp(dp), .AN(AN));
+    Decoder10to32 decoder(.SW_in(SW_in[9:0]), .out(SW));
+    seg7_driver driver(.clk(clk), .rst_n(SW_in[10]), .SW(SW), .Cnode(Cnode), .dp(dp), .AN(AN));
     
-    assign LEDoutput = switchInput;
+    assign led = SW_in;
 
 endmodule
