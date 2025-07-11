eaee1deb57f0d661f0bfd83210e1f727f5c765b6
paulk4100 <paulkim@cpp.edu>
2025-07-11 00:33:24 -0700

    Update seg7_driver.v

--- Group E/ECE3300-Lab4-GroupE/seg7_driver.v
+++ Group E/ECE3300-Lab4-GroupE/seg7_driver.v
@@ -62,11 +62,10 @@ module seg7_driver(
     reg [7:0] AN_tmp;
     always@(s)
         case(s)
-            3'd0:AN_tmp=8'b11111110;3'd1:AN_tmp=8'b11111101;
-            3'd2:AN_tmp=8'b11111011;3'd3:AN_tmp=8'b11110111;
-            3'd4:AN_tmp=8'b11101111;3'd5:AN_tmp=8'b11011111;
-            3'd6:AN_tmp=8'b10111111;3'd7:AN_tmp=8'b01111111;
-            default:AN_tmp=8'b11111111;
+            3'd0:digit=SW[3:0]; 3'd1:digit=SW[7:4];
+            3'd2:digit=SW[11:8]; 3'd3:digit=SW[15:12];
+            3'd4:digit=SW[3:0]; 3'd5:digit=SW[7:4];
+            3'd6:digit=SW[11:8]; 3'd7:digit=SW[15:12];
         endcase
         
     assign AN=AN_tmp;
