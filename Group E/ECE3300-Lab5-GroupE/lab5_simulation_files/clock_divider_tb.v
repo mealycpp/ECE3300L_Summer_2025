bfddcd841867a4dc06f039f4efe06766500d0d8e
paulk4100 <paulkim@cpp.edu>
2025-07-16 12:00:06 -0700

    Update clock_divider_tb.v

--- Group E/ECE3300-Lab5-GroupE/lab5_simulation_files/clock_divider_tb.v
+++ Group E/ECE3300-Lab5-GroupE/lab5_simulation_files/clock_divider_tb.v
@@ -1,4 +1,4 @@
-
+`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
 // Engineer: ECE
@@ -19,3 +19,24 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+module clock_divider_tb();
+    reg clk = 0;
+    reg rst_n;
+    wire [31:0] cnt;
+
+    clock_divider uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .cnt(cnt)
+    );
+
+    always #5 clk = ~clk;  // 10 ns period
+
+    initial begin
+        rst_n = 0;
+        #20;
+        rst_n = 1;
+        #500;
+        $finish;
+    end
+endmodule
