2de028a7177be1205dedf45d550ca4dd7b202a5a
paulk4100 <paulkim@cpp.edu>
2025-07-16 11:59:00 -0700

    Update clock_divider_tb.v

--- Group E/ECE3300-Lab5-GroupE/lab5_simulation_files/clock_divider_tb.v
+++ Group E/ECE3300-Lab5-GroupE/lab5_simulation_files/clock_divider_tb.v
@@ -1,1 +1,21 @@
 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly Pomona
+// Engineer: ECE
+// 
+// Create Date: 07/16/2025 11:58:25 AM
+// Design Name: 
+// Module Name: clock_divider_tb
+// Project Name: clock_divider_tb
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
