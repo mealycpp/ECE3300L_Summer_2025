8300abb25537aea13580835a1d32e83394dc4d40
EdwinEstrada102 <87724218+EdwinEstrada102@users.noreply.github.com>
2025-07-20 22:57:41 -0700

    Add files via upload

---
+++ Group A/ECE3300L-Lab5-Group A/Verilog Source Files and Testbench Code/Verilog Source Code/clock_divider.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module clock_divider (
+    input clk,              
+    input rst_n,            
+    output reg [31:0] cnt   
+);
+    always @(posedge clk or negedge rst_n)
+        if (!rst_n)
+            cnt <= 32'd0;
+        else
+            cnt <= cnt + 1;
+endmodule
