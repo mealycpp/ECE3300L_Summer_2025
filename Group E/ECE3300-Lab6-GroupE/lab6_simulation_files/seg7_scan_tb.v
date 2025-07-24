4d249bdf2136c2ad8fc31fb21638aef22057a511
paulk4100 <paulkim@cpp.edu>
2025-07-24 12:03:06 -0700

    Update seg7_scan_tb.v

--- Group E/ECE3300-Lab6-GroupE/lab6_simulation_files/seg7_scan_tb.v
+++ Group E/ECE3300-Lab6-GroupE/lab6_simulation_files/seg7_scan_tb.v
@@ -1,12 +1,11 @@
-module seg7_scan_tb (
-  reg clk = 0,
-  reg rst_n = 0,
-  reg [3:0] digit0 = 4'd5,
-  reg [3:0] digit1 = 4'd9,
-  reg [3:0] digit2 = 4'd3,
-  wire [6:0] seg,
-  wire [2:0] an
-);
+module seg7_scan_tb;
+  reg clk = 0;
+  reg rst_n = 0;
+  reg [3:0] digit0 = 4'd5;
+  reg [3:0] digit1 = 4'd9;
+  reg [3:0] digit2 = 4'd3;
+  wire [6:0] seg;
+  wire [2:0] an;
   
   seg7_scan uut (
     .clk(clk),
