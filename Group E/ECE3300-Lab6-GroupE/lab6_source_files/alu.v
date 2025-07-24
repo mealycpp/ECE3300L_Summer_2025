22ea2ca8509e05f34f4575b6108e2e51e2e00c6c
paulk4100 <paulkim@cpp.edu>
2025-07-24 11:20:46 -0700

    Update alu.v

--- Group E/ECE3300-Lab6-GroupE/lab6_source_files/alu.v
+++ Group E/ECE3300-Lab6-GroupE/lab6_source_files/alu.v
@@ -1,8 +1,27 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly Pomona
+// Engineer: ECE
+// 
+// Create Date: 07/24/2025 11:20:20 AM
+// Design Name: 
+// Module Name: alu
+// Project Name: alu
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module alu(
-    input [3:0] A, B,      // BCD values from counters
-    input [1:0] ctrl,      // ALU control: 00 = add, 01 = sub
+    input [3:0] A, B,      
+    input [1:0] ctrl,      
     output reg [7:0] result
 );
     always @(*) begin
