cd2bdd290b154d90b31409f268001b061638ae85
paulk4100 <paulkim@cpp.edu>
2025-07-24 11:32:41 -0700

    Update control_decoder.v

--- Group E/ECE3300-Lab6-GroupE/lab6_source_files/control_decoder.v
+++ Group E/ECE3300-Lab6-GroupE/lab6_source_files/control_decoder.v
@@ -1,4 +1,23 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Cal Poly Pomona
+// Engineer: ECE
+// 
+// Create Date: 07/24/2025 11:28:54 AM
+// Design Name: 
+// Module Name: control_decoder
+// Project Name: control_decoder
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module control_decoder(
     input [3:0] ctrl_in,         // {SW8, SW7, SW6, SW5}
