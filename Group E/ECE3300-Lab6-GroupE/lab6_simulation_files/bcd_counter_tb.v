ec0d06d29509dad6dcc21dc1be5c74fd6a7c9aaa
paulk4100 <paulkim@cpp.edu>
2025-07-24 11:47:54 -0700

    Update bcd_counter_tb.v

--- Group E/ECE3300-Lab6-GroupE/lab6_simulation_files/bcd_counter_tb.v
+++ Group E/ECE3300-Lab6-GroupE/lab6_simulation_files/bcd_counter_tb.v
@@ -1,1 +1,20 @@
 
+module bcd_counter_tb (
+    input clk,
+    input rst_n,
+    input dir,            // 1 = up, 0 = down
+    output reg [3:0] bcd
+);
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            bcd <= 0;
+        else begin
+            if (dir) begin
+                bcd <= (bcd == 9) ? 0 : bcd + 1;
+            end else begin
+                bcd <= (bcd == 0) ? 9 : bcd - 1;
+            end
+        end
+    end
+endmodule
