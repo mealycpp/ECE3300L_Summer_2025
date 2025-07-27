333c50f6ca66a55fedc82e27e4ec8a3688801003
HGaribay00 <hdgaribay@cpp.edu>
2025-07-27 13:29:47 -0700

    Add files via upload

---
+++ Group S/ECE3300- Lab6- GroupS/alu.v
@@ -0,0 +1,14 @@
+module alu(
+input [3:0] A,
+input [3:0] B,
+input [1:0] ctrl,
+output reg [7:0] result
+);
+always @(*) begin
+case (ctrl) 
+2'b00: result = A+B;
+2'b01: result = A-B;
+default: result = 0;
+endcase
+end
+endmodule
