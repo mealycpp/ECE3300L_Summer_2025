e285ee5752a02434fb97d0884535a7d9c797a9e2
SnakeJared <jaredmocs@gmail.com>
2025-07-29 15:02:56 -0700

    Update bcd_counter.v

--- Group Q/ECE3300-Lab6-GroupQ/Sources/bcd_counter.v
+++ Group Q/ECE3300-Lab6-GroupQ/Sources/bcd_counter.v
@@ -1,1 +1,38 @@
 
+module bcd_counter(
+    input clk_div,
+    input rst_n,           // Active-low reset (BTN0)
+    input dir_bit,         // 1 = count up, 0 = count down (SW [8:7])
+    output reg [3:0] digit // Ones digit in BCD
+
+
+    );
+
+
+    always@(posedge clk_div or negedge rst_n) begin
+      
+        if (!rst_n) begin
+            digit <= 4'b0 ;
+            end
+        else begin 
+          if (!dir_bit) begin
+            if (digit == 4'd9) begin      
+            digit <= 4'd0;
+
+            end
+            else
+            digit <= digit + 1;
+            end
+          else begin
+            if (digit == 4'd0) begin     
+            digit <= 4'd9;
+          
+            end
+            else
+            digit <= digit - 1;
+            end
+
+         end
+        end
+
+endmodule
