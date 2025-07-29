0001c7d377e79587c5f3b5bc5d780b6d102be8a7
SnakeJared <jaredmocs@gmail.com>
2025-07-29 15:04:39 -0700

    Update control_decoder.v

--- Group Q/ECE3300-Lab6-GroupQ/Sources/control_decoder.v
+++ Group Q/ECE3300-Lab6-GroupQ/Sources/control_decoder.v
@@ -1,1 +1,10 @@
+module control_decoder(
+  input [3:0] controlSW, // {SW [8-5]} 
+    output reg [3:0] ctrl_nibble
+    );
 
+    always@(*)begin
+        ctrl_nibble = controlSW;
+    end
+
+endmodule
