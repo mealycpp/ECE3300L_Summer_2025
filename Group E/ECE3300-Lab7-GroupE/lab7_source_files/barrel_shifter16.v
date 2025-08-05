782906d76296508131b4226adb3c8492a3220509
paulk4100 <paulkim@cpp.edu>
2025-08-05 11:05:21 -0700

    Update barrel_shifter16.v

--- Group E/ECE3300-Lab7-GroupE/lab7_source_files/barrel_shifter16.v
+++ Group E/ECE3300-Lab7-GroupE/lab7_source_files/barrel_shifter16.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: Cal Poly Pomona
-// Engineer: ECE
+// Company: 
+// Engineer: 
 // 
-// Create Date: 08/04/2025 10:53:14 AM
+// Create Date: 08/05/2025 10:54:00 AM
 // Design Name: 
 // Module Name: barrel_shifter16
-// Project Name: barrel_shifter16
+// Project Name: 
 // Target Devices: 
 // Tool Versions: 
 // Description: 
@@ -20,36 +20,66 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module barrel_shifter16(
-    input wire [15:0] data_in,
-    input wire [3:0] shamt,
-    input wire dir,       // 0 = LEFT, 1 = RIGHT
-    input wire rotate,    // 0 = LOGICAL, 1 = ROTATE
+    input  wire [15:0] data_in,
+    input  wire [3:0]  shamt,     
+    input  wire        dir,       
+    input  wire        rotate,    
     output wire [15:0] data_out
 );
-    wire [15:0] stage[4:0];
-    assign stage[0] = data_in;
+    wire [15:0] stage1, stage2, stage3, stage4;
 
-    genvar i;
-    generate
-        for (i = 0; i < 4; i = i + 1) begin : stages
-            wire [15:0] in = stage[i];
-            wire [15:0] shifted;
+    // Stage shift amount = 1
+    assign stage1 = shamt[0] ?
+        (dir ?
+            // right
+            (rotate ? {data_in[0],      data_in[15:1]}  // ror 1
+                    : {1'b0,            data_in[15:1]}) // >> 1 logical
+          :
+            // left
+            (rotate ? {data_in[14:0],   data_in[15]}    // rol 1
+                    : {data_in[14:0],   1'b0})          // << 1 logical
+        )
+        : data_in;
 
-            if (dir == 1'b0) begin
-                // LEFT shift/rotate
-                assign shifted = rotate ?
-                    {in[15 - (1<<i):0], in[15:16 - (1<<i)]} :
-                    {in[15 - (1<<i):0], {(1<<i){1'b0}}};
-            end else begin
-                // RIGHT shift/rotate
-                assign shifted = rotate ?
-                    {in[(1<<i)-1:0], in[15:(1<<i)]} :
-                    {{(1<<i){1'b0}}, in[15:(1<<i)]};
-            end
+    // Stage shift amount = 2
+    assign stage2 = shamt[1] ?
+        (dir ?
+            // right
+            (rotate ? {stage1[1:0],     stage1[15:2]}   // ror 2
+                    : {2'b00,           stage1[15:2]})  // >> 2 logical
+          :
+            // left
+            (rotate ? {stage1[13:0],    stage1[15:14]}  // rol 2
+                    : {stage1[13:0],    2'b00})         // << 2 logical
+        )
+        : stage1;
 
-            assign stage[i+1] = shamt[i] ? shifted : in;
-        end
-    endgenerate
+    // Stage shift amount = 4
+    assign stage3 = shamt[2] ?
+        (dir ?
+            // right
+            (rotate ? {stage2[3:0],     stage2[15:4]}   // ror 4
+                    : {4'b0000,         stage2[15:4]})  // >> 4 logical
+          :
+            // left
+            (rotate ? {stage2[11:0],    stage2[15:12]}  // rol 4
+                    : {stage2[11:0],    4'b0000})       // << 4 logical
+        )
+        : stage2;
 
-    assign data_out = stage[4];
+    // Stage shift amount = 8
+    assign stage4 = shamt[3] ?
+        (dir ?
+            // right
+            (rotate ? {stage3[7:0],     stage3[15:8]}   // ror 8
+                    : {8'b0000_0000,    stage3[15:8]})  // >> 8 logical
+          :
+            // left
+            (rotate ? {stage3[7:0],     stage3[15:8]}   // rol 8 (same wiring as ror 8 on 16-bit)
+                    : {stage3[7:0],     8'b0000_0000})  // << 8 logical
+        )
+        : stage3;
+
+    assign data_out = stage4;
+
 endmodule
