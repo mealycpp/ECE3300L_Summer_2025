4c25c89ed0c225e123079a247bd3f5547ff3713b
paulk4100 <paulkim@cpp.edu>
2025-08-05 11:54:42 -0700

    Update seg7_scan8_tb.v

--- Group E/ECE3300-Lab7-GroupE/lab7_simulation_files/seg7_scan8_tb.v
+++ Group E/ECE3300-Lab7-GroupE/lab7_simulation_files/seg7_scan8_tb.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+`timescale 1us / 1ns   
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
 // Engineer: ECE
@@ -21,32 +21,40 @@
 
 module seg7_scan8_tb;
 
-    reg clk = 0;
-    wire [7:0] an;
-    wire [6:0] seg;
+    reg        clk = 0;            
+    wire [7:0] AN;                
+    wire [6:0] SEG;          
 
-    reg [6:0] hex_seg [7:0];
+    reg [6:0] d0, d1, d2, d3, d4, d5, d6, d7;
 
-    seg7_scan8 uut (
-        .clk(clk),
-        .hex_seg(hex_seg),
-        .an(an),
-        .seg(seg)
+    always #500 clk = ~clk;
+
+    seg7_scan8 dut (
+        .clk_1kHz (clk),
+        .d0       (d0),
+        .d1       (d1),
+        .d2       (d2),
+        .d3       (d3),
+        .d4       (d4),
+        .d5       (d5),
+        .d6       (d6),
+        .d7       (d7),
+        .AN       (AN),
+        .SEG      (SEG)
     );
 
-    always #500 clk = ~clk; // 1 kHz
-
     initial begin
-        hex_seg[0] = 7'b1000000;
-        hex_seg[1] = 7'b1111001;
-        hex_seg[2] = 7'b0100100;
-        hex_seg[3] = 7'b0110000;
-        hex_seg[4] = 7'b0011001;
-        hex_seg[5] = 7'b0010010;
-        hex_seg[6] = 7'b0000010;
-        hex_seg[7] = 7'b1111000;
+        d0 = 7'b1000000; // 0
+        d1 = 7'b1111001; // 1
+        d2 = 7'b0100100; // 2
+        d3 = 7'b0110000; // 3
+        d4 = 7'b0011001; // 4
+        d5 = 7'b0010010; // 5
+        d6 = 7'b0000010; // 6
+        d7 = 7'b1111000; // 7
 
-        #10000;
+        #20000; // 20 ms
         $finish;
     end
 endmodule
+
