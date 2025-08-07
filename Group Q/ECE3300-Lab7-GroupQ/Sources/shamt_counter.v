33293c1b25a703a29d2739e5f3b04bc4fab3c53a
keevanese <kqtang@cpp.edu>
2025-08-06 22:27:07 -0700

    Update shamt_counter.v

--- Group Q/ECE3300-Lab7-GroupQ/Sources/shamt_counter.v
+++ Group Q/ECE3300-Lab7-GroupQ/Sources/shamt_counter.v
@@ -1,12 +1,15 @@
+`timescale 1ns / 1ps
+
 module shamt_counter(
-    input clk, rst,
-    input btn_inc,      // from BTNC debounced
-    output reg [1:0] shamt_hi
-);
-    always @(posedge clk or posedge rst) begin
-        if (rst)
-            shamt_hi <= 0;
-        else if (btn_inc)
-            shamt_hi <= shamt_hi + 1;
-    end
+                     input wire clk,
+                     input BTNC,
+                     output reg [1:0] shamt_hi
+                    );
+                    
+                    reg btnc_last = 0;
+                    always @(posedge clk) begin
+                        btnc_last <= BTNC;
+                        if (BTNC & ~btnc_last)
+                            shamt_hi <= shamt_hi + 1;
+                    end
 endmodule
