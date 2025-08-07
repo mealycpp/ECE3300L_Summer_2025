923b7bc69dfcfd1a472f054bad6fd323c4b6eed1
keevanese <kqtang@cpp.edu>
2025-08-06 22:25:53 -0700

    Update debounce_toggle.v

--- Group Q/ECE3300-Lab7-GroupQ/Sources/debounce_toggle.v
+++ Group Q/ECE3300-Lab7-GroupQ/Sources/debounce_toggle.v
@@ -1,19 +1,22 @@
 module debounce_toggle(
-    input clk,       // ~1kHz
-    input btn_raw,
-    output reg btn_toggle = 0
+    input wire clk_1khz,       
+    input wire btn_raw,       
+    output reg btn_toggle = 0 
 );
-    reg [2:0] shift = 3'b000;
-    reg state = 0;
-    always @(posedge clk) begin
-        shift <= {shift[1:0], btn_raw};
-        if (&shift) begin
-            if (!state) begin
-                btn_toggle <= ~btn_toggle;
-                state <= 1;
-            end
-        end else if (~|shift) begin
-            state <= 0;
-        end
+
+    reg [2:0] shift_reg = 3'b000;
+    reg debounced = 0;
+    reg prev_debounced = 0;
+
+    always @(posedge clk_1khz) begin
+        shift_reg <= {shift_reg[1:0], btn_raw};
+        if (shift_reg == 3'b111)        // Debounced value = 1 if all 3 bits are high, 0 if all 3 bits are low
+            debounced <= 1;
+        else if (shift_reg == 3'b000)
+            debounced <= 0;
+        if (~prev_debounced && debounced)    
+            btn_toggle <= ~btn_toggle;
+
+        prev_debounced <= debounced;         // Save previous debounced state
     end
 endmodule
