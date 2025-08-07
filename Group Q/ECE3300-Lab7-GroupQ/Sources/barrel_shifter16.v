5944c36c7deff870fc10762c3b464041e0218423
keevanese <kqtang@cpp.edu>
2025-08-06 22:24:58 -0700

    Update barrel_shifter16.v

--- Group Q/ECE3300-Lab7-GroupQ/Sources/barrel_shifter16.v
+++ Group Q/ECE3300-Lab7-GroupQ/Sources/barrel_shifter16.v
@@ -1,23 +1,35 @@
 module barrel_shifter16(
-    input  [15:0] data_in,
-    input  [3:0]  shamt,
-    input         dir,    // 0 = left, 1 = right
-    input         rotate, // 0 = logical, 1 = rotate
-    output [15:0] data_out
+    input wire [15:0] data_in,                                                             
+    input wire [3:0] shamt,                                                           
+    input wire dir,                                                                           
+    input wire rotate,                                                                       
+    output wire [15:0] data_out                                                            
 );
-    reg [15:0] temp;
-    always @(*) begin
-        if (dir == 0) begin
-            if (rotate)
-                temp = (data_in << shamt) | (data_in >> (16 - shamt));
-            else
-                temp = data_in << shamt;
-        end else begin
-            if (rotate)
-                temp = (data_in >> shamt) | (data_in << (16 - shamt));
-            else
-                temp = data_in >> shamt;
-        end
-    end
-    assign data_out = temp;
+    wire [15:0] s_stage1, s_stage2, s_stage3, s_stage4;                                        
+
+    // shift/rotate by 1
+    assign s_stage1 = (shamt[0]) ?                                                              
+        (rotate ? (dir ? {data_in[0], data_in[15:1]} : {data_in[14:0], data_in[15]}) :
+                  (dir ? {1'b0, data_in[15:1]}       : {data_in[14:0], 1'b0})) :
+        data_in;
+
+    // shift/rotate by 2
+    assign s_stage2 = (shamt[1]) ?
+        (rotate ? (dir ? {s_stage1[1:0], s_stage1[15:2]} : {s_stage1[13:0], s_stage1[15:14]}) :
+                  (dir ? {2'b00, s_stage1[15:2]}         : {s_stage1[13:0], 2'b00})) :
+        s_stage1;
+
+    // shift/rotate by 4
+    assign s_stage3 = (shamt[2]) ?
+        (rotate ? (dir ? {s_stage2[3:0], s_stage2[15:4]} : {s_stage2[11:0], s_stage2[15:12]}) :
+                  (dir ? {4'b0000, s_stage2[15:4]}       : {s_stage2[11:0], 4'b0000})) :
+        s_stage2;
+
+    // shift/rotate by 8
+    assign s_stage4 = (shamt[3]) ?
+        (rotate ? (dir ? {s_stage3[7:0], s_stage3[15:8]} : {s_stage3[7:0], s_stage3[15:8]}) :
+                  (dir ? {8'b00000000, s_stage3[15:8]}   : {s_stage3[7:0], 8'b00000000})) :
+        s_stage3;
+
+    assign data_out = s_stage4;                                                                 // Final shifted/rotated result
 endmodule
