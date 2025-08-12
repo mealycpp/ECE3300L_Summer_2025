38b53692b279a016797ac11dcf38fe5f8d756dde
cbmusonda <cbmusonda@cpp.edu>
2025-08-12 12:19:08 -0700

    Update rgb_led_driver.v

--- Group F/ECE3300-LAB8-Group F/Verilog Code/rgb_led_driver.v
+++ Group F/ECE3300-LAB8-Group F/Verilog Code/rgb_led_driver.v
@@ -20,16 +20,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module rgb_led_driver #(parameter ACTIVE_LOW=1)(
-    input wire pwm_r, pwm_g, pwm_b,
+module rgb_led_driver #(
+    parameter ACTIVE_LOW = 1  // Nexys A7 RGB LEDs are active-low
+)(
+    input  wire pwm_r, pwm_g, pwm_b,
     output wire led_r, led_g, led_b
 );
-    generate    
+    generate
         if (ACTIVE_LOW) begin
             assign led_r = ~pwm_r;
             assign led_g = ~pwm_g;
             assign led_b = ~pwm_b;
-        end else begin 
+        end else begin
             assign led_r = pwm_r;
             assign led_g = pwm_g;
             assign led_b = pwm_b;
