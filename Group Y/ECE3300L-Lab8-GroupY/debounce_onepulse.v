c320d1fb2200ba7cdd0a74b4fdf69663f3154d52
seanwygant <seanwygant@gmail.com>
2025-08-14 21:32:43 -0700

    Add files via upload

---
+++ Group Y/ECE3300L-Lab8-GroupY/debounce_onepulse.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/13/2025 05:48:25 PM
+// Design Name: 
+// Module Name: debounce_onepulse
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module debounce_onepulse #(parameter integer STABLE_TICKS = 20)
+(
+input wire clk,
+input wire rst_n,
+input wire din,
+output reg pulse
+);
+reg d0, d1;
+reg stable, stable_q;
+reg [$clog2(STABLE_TICKS+1)-1:0] cnt;
+always @(posedge clk or negedge rst_n) begin
+if (!rst_n) begin d0<=0; d1<=0; end else begin d0<=din; d1<=d0; end
+end
+always @(posedge clk or negedge rst_n) begin
+if (!rst_n) begin cnt<=0; stable<=0; end
+else if (d1 != stable) begin
+if (cnt==STABLE_TICKS) begin stable<=d1; cnt<=0; end
+else cnt<=cnt+1;
+end else cnt<=0;
+end
+always @(posedge clk or negedge rst_n) begin
+if (!rst_n) begin stable_q<=0; pulse<=0; end
+else begin pulse <= (~stable_q) & stable; stable_q <= stable; end
+end
+endmodule
---
+++ Group Y/ECE3300L-Lab8-GroupY/rgb_led_driver.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/13/2025 05:49:17 PM
+// Design Name: 
+// Module Name: rgb_led_driver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rgb_led_driver #(parameter ACTIVE_LOW=1)(
+input wire pwm_r, pwm_g, pwm_b,
+output wire led_r, led_g, led_b
+);
+generate
+if (ACTIVE_LOW) begin
+assign led_r = ~pwm_r;
+assign led_g = ~pwm_g;
+assign led_b = ~pwm_b;
+end else begin
+assign led_r = pwm_r;
+assign led_g = pwm_g;
+assign led_b = pwm_b;
+end
+endgenerate
+endmodule
